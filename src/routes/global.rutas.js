4c5901a5ad818b6eabb28bde12b75617362fe58e
Javier Barker <hernandez.javier2003@gmail.com>
2021-03-12 23:29:52 -0600

    Elimine archivos inecesarios
    Elimine archivos inecesarios

--- src/routes/global.rutas.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const express = require("express");
-const globalControlador = require("../controllers/global.controlador");
-
-var api = express.Router();
-
-api.post('/login', globalControlador.login);
-
-module.exports = api;
